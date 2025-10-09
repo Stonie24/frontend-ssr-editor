25cd32e2dfabc96f304e3b2024519818049b045e
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-10-09 10:46:43 +0200

    Removed navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Login</router-link>
-      <router-link to="/docs">Docs</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
